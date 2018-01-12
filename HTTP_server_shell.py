e92e97ef20545bf21bf0baefd70dce9ad8d88172
adi bleyer <bleyer23@gmail.com>
2018-01-12 10:36:14 +0200

    improve valid funcsion

--- HTTP_server_shell.py
+++ HTTP_server_shell.py
@@ -1,6 +1,6 @@
 """
  HTTP Server Shell
- Author: Barak Gonen and Nir Dweck
+ Author: adi bleyer
  Purpose: Provide a basis for Ex. 4
  Note: The code is written in a simple way, without classes, log files or
  other utilities, for educational purpose
@@ -11,15 +11,19 @@ import socket
 import os
 # TO DO: set constants
 
-DEFEALT_FILE = '\\index.html'
+DEFEALT_FILE = '/index.html'
 HTTP_VERSION = 'HTTP/1.1'
-DEFAULT_URL = "D:\\adi\\Documents\\pyton\\HTTP_server\\webroot"
+DEFAULT_URL = "D:\\adi\\Documents\\python\\HTTP_server\\webroot\\"
 REQUEST_CODE = 'GET'
-QUEUE_SIZE = 10
+QUEUE_SIZE = 1
+BAD_REQUEST = '400'
+NOT_FOUND = '404'
 IP = '0.0.0.0'
 PORT = 80
 SOCKET_TIMEOUT = 2
-
+FILE_TYPES_HEADER = {'Html': 'text/html;charset=utf-8', 'jpg': 'image/jpeg', 'text/css': 'css',
+                     'js': 'text/javascript; charset=UTF-8', 'txt': 'text/plain', 'ico': "image/x-icon",
+                     'gif': 'image/jpeg', 'png': 'image/png'}
 
 def get_file_data(file_name):
     """
@@ -42,9 +46,7 @@ def handle_client_request(resource, client_socket):
     :param client_socket: a socket for the communication with the client
     :return: None
     """
-"""
-    # TO DO : add code that given a resource (URL and parameters) generates
-    # the proper response
+    http_header = HTTP_VERSION + " "
     if resource == '':
         uri = DEFAULT_URL + DEFEALT_FILE
     else:
@@ -53,21 +55,21 @@ def handle_client_request(resource, client_socket):
     # TO DO: check if URL had been redirected, not available or other error
     # code. For example:
     if url in REDIRECTION_DICTIONARY:
+        http_header += '302 FOUND'
         # TO DO: send 302 redirection response
+    http_header += '\r\n'
 
     # TO DO: extract requested file tupe from URL (html, jpg etc)
-    if file_type == 'html':
-        http_header = # TO DO: generate proper HTTP header
-    elif file_type == 'jpg':
-        http_header = # TO DO: generate proper jpg header
+    http_header += FILE_TYPES_HEADER.get(file_type)
     # TO DO: handle all other headers
 
     # TO DO: read the data from the file
     data = get_file_data(filename)
+    http_header += '\r\n'
     http_response = http_header + data
     client_socket.send(http_response)
 
-"""
+
 def validate_http_request(request):
     """
     done + ceacked
@@ -78,18 +80,27 @@ def validate_http_request(request):
     the requested resource )
     """
     # TO DO: write function
-    fildes = request.split(' ')
-    if fildes[1] == '\\':
-        fildes[1] = DEFAULT_URL + DEFEALT_FILE
+    fileds = request.split('\r\n')
+    request_line = fileds[0].split(' ')
+    print fileds
+    if not (fileds[1] == "" and fileds[-1] == ""):
+        print 'not end line'
+        return False, BAD_REQUEST
+    print request_line
+    if request_line[1] == '/':
+        request_line[1] = DEFAULT_URL + DEFEALT_FILE
     else:
-        fildes[1] = DEFAULT_URL + fildes[1]
-    if not fildes[0] == REQUEST_CODE:
-        return False
-    if not os.path.exists(fildes[1]):
-        return False
-    if not fildes[2] == HTTP_VERSION + '\r\n':
-        return False
-    return True, fildes
+        request_line[1] = DEFAULT_URL + request_line[1]
+    if not request_line[0] == REQUEST_CODE:
+        print 'dont get'
+        return False, BAD_REQUEST
+    if not os.path.exists(request_line[1]):
+        print 'path dont exzist'
+        return False, NOT_FOUND
+    if not request_line[2] == HTTP_VERSION:
+        print 'not version'
+        return False, BAD_REQUEST
+    return True, fileds
 
 
 
@@ -141,9 +152,9 @@ def main():
 
 
 if __name__ == "__main__":
-    assert validate_http_request('GET \\ HTTP/1.1\r\n')
-    assert not validate_http_request('GEV \\ HTTP/1.2\r\n')
-    assert not validate_http_request('GET \\vv HTTP/1.1\r\n')
-    assert validate_http_request('GET \\css\\doremon.css HTTP/1.1\r\n')
+    assert validate_http_request('GET / HTTP/1.1\r\n')[0]
+    assert not validate_http_request('GEV / HTTP/1.2\r\n')[0]
+    assert not validate_http_request('GET /vv HTTP/1.1\r\n')[0]
+    assert validate_http_request('GET /css\\doremon.css HTTP/1.1\r\n')[0]
     main()
 
