d58b81fccd6e43d5941628526ecb20e150b42338
adi bleyer <bleyer23@gmail.com>
2018-01-19 13:38:08 +0200

    cange funcsions

--- HTTP_server_shell.py
+++ HTTP_server_shell.py
@@ -8,7 +8,7 @@ import os
 
 DEFEALT_FILE = '/index.html'
 HTTP_VERSION = 'HTTP/1.1'
-DEFAULT_URL = "D:\\adi\\Documents\\python\\HTTP_server\\webroot\\"
+DEFAULT_URL = "webroot\\"
 REQUEST_CODE = 'GET'
 QUEUE_SIZE = 10
 MOVED_REQUEST = '302 MOVED TEMPORARILY'
@@ -33,10 +33,20 @@ ENT_HTTP_CHARS = '\r\n\r\n'
 TYPE_HEADER = 'Content-Type:'
 LENGTH_HEADER = 'Content-Length:'
 LOCATION_HEADER = 'Location:/'
+VALID_PARAMS = ['num']
+
+
+def handel_params(params):
+    """
+    handel all the params
+    :return: (TRUE/FALSE,data/error code)
+    """
+    pass
 
 
 def recv_http(client_socket):
     """
+    need cange
     recv an http request
     :client_socket: the client socket
     :return: return the http request that received from the client
@@ -59,7 +69,7 @@ def get_file_data(file_name):
     return text
 
 
-def handle_client_request(resource):
+def handel_file_sent(resource):
     """
     get the http valid request
     Check the required resource, generate proper HTTP response and send
@@ -67,18 +77,16 @@ def handle_client_request(resource):
     :param resource: the required resource
     :return: http request ready to send
     """
+    print resource
     data = ""
     http_header = HTTP_VERSION + ENF_FILED_CHAR
-    is_valid, path = resource
-    if not is_valid:
-        http_header += path + END_LINE_CHAR
-    else:
-        file_type = path.split('.')[-1]
-        http_header += VALID_REQUEST + END_LINE_CHAR
-        http_header += TYPE_HEADER + FILE_TYPES_HEADER.get(file_type)
-        http_header += END_LINE_CHAR
-        data = get_file_data(resource[1])
-        http_header += LENGTH_HEADER + str(len(data))
+    path = resource
+    file_type = path.split('.')[-1]
+    http_header += VALID_REQUEST + END_LINE_CHAR
+    http_header += TYPE_HEADER + FILE_TYPES_HEADER.get(file_type)
+    http_header += END_LINE_CHAR
+    data = get_file_data(resource)
+    http_header += LENGTH_HEADER + str(len(data))
     http_header += ENT_HTTP_CHARS
     http_response = http_header + data
     return http_response
@@ -90,40 +98,40 @@ def valid_URI(uri):
     :uri: the URI
     :return: tuple of (TRUE/FALSE,path/error type)
     """
+    print 'hh'+uri
     if uri == '/':
         return True, DEFAULT_URL + DEFEALT_FILE[1:]
     elif uri in UNIQUE_URI:
-        return False, UNIQUE_URI.get(uri)
+        print 'unick'
+        return True, UNIQUE_URI.get(uri)
     else:
         uri = uri.replace('/', '\\')
-        return True, DEFAULT_URL + uri[1:]
+        if os.path.isfile(DEFAULT_URL + uri):
+            return True, DEFAULT_URL + uri[1:]
+        else:
+            return False, NOT_FOUND
 
 
-def validate_http_request(request):
+def read_request(request):
     """
     Check if request is a valid HTTP request and returns TRUE / FALSE and
     the extra data
     :param request: the request which was received from the client
-    :return: a tuple of (True/False - depending if the request is valid,
-    the requested resource ).
+    :return: a tuple of (True/False - depending if you need to send file or not + extra data
     if true add to the requested resource a path to the wanted file.
+    if false add the ststus code and string.
     """
     fileds = request.split(END_LINE_CHAR)
     fileds[0] = fileds[0].split(ENF_FILED_CHAR)
     request_line = fileds[0][:]
-    is_valid, path = valid_URI(request_line[1])
-    if not is_valid:
-        return False, path
-    elif not fileds.pop() == '':
-        return False, BAD_REQUEST
+    if not fileds.pop() == '':
+        return False
     elif not request_line[0] == REQUEST_CODE:
-        return False, BAD_REQUEST
-    elif not os.path.isfile(path):
-        return False, NOT_FOUND
+        return False
     elif not request_line[2] == HTTP_VERSION:
-        return False, BAD_REQUEST
+        return False
     else:
-        return True, (VALID_REQUEST, path)
+        return True
 
 
 def handle_client(client_socket):
@@ -136,19 +144,32 @@ def handle_client(client_socket):
     print 'Client connected'
     while True:
         request = recv_http(client_socket)
-        valid_http, resource = validate_http_request(request)
-        if valid_http:
+        is_valid = read_request(request)
+        is_valid_uri, resource = valid_URI(request.split('\r\n')[0].split(' ')[1])
+        if is_valid and is_valid_uri:
             print 'Got a valid HTTP request'
-            http_response = handle_client_request(resource)
+            print request
+            if resource == MOVED_REQUEST:
+                http_response = HTTP_VERSION + ENF_FILED_CHAR + resource + END_LINE_CHAR
+                http_response += LOCATION_HEADER + END_LINE_CHAR
+                http_response += ENF_FILED_CHAR
+                print resource
+            elif os.path.isfile(resource):
+                http_response = handel_file_sent(resource)
+            else:
+                http_response = HTTP_VERSION + ENF_FILED_CHAR + resource
+                http_response += END_LINE_CHAR
+                http_response += END_LINE_CHAR
             err = client_socket.sendall(http_response)
+
             if err is not None:
                 print 'err:'+err
         else:
             print 'Error: Not a valid HTTP request'
-            http_response = HTTP_VERSION + ENF_FILED_CHAR + resource
+            print is_valid
+            print is_valid_uri
+            http_response = HTTP_VERSION + resource + BAD_REQUEST
             http_response += END_LINE_CHAR
-            if resource == MOVED_REQUEST:
-                http_response += LOCATION_HEADER
             http_response += END_LINE_CHAR
             client_socket.sendall(http_response)
     print 'Closing connection'
@@ -180,9 +201,8 @@ def main():
 
 
 if __name__ == "__main__":
-    assert validate_http_request('GET / HTTP/1.1\r\n\r\n')[0]
-    assert not validate_http_request('GEV / HTTP/1.2\r\n\r\n')[0]
-    assert not validate_http_request('GET /vv HTTP/1.1\r\n\r\n')[0]
-    assert validate_http_request('GET /css\\doremon.css HTTP/1.1'
-                                 '\r\nUpgrade-Insecure-Requests: 1\r\n\r\n')[0]
+    assert read_request('GET / HTTP/1.1\r\n\r\n')
+    assert not read_request('GEV / HTTP/1.2\r\n\r\n')
+    assert not valid_URI('/vv')[0]
+    assert read_request('GET /css\\doremon.css HTTP/1.1\r\nUpgrade-Insecure-Requests: 1\r\n\r\n')
     main()
